2f4f1f1c0d06c4f869cb1244924f7fc68a458f8b
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-06-28 14:38:02 -0400

    portfolio link change

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,7 +6,7 @@ export default function Info() {
             <h1> Katherine Marcott </h1> 
             <h2> Software Engineer </h2> 
             <h3> Katherinesoftwareengineer@gmail.com </h3>
-            <button className="info--btn-portfolio btn"> <a href="#" rel="noreferrer" target="_blank"> Portfolio </a></button>
+            <button className="info--btn-portfolio btn"> <a href="https://www.linkedin.com/in/katherinemarcott/" rel="noreferrer" target="_blank"> Portfolio </a></button>
             <button className="info--btn-linkedin btn"> <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/katherinemarcott/"><img src="./images/Vector.png" alt="LinkedIn"/> LinkedIn </a> </button>
         </section>
     )
