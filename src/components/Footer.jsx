4b90ce9e7b18bf9d18ab88c4044a9ec69523417b
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-06-27 23:41:51 -0400

    add rel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer>
             <ul>
-                <li> <a target="_blank" href="https://twitter.com/CodingKatCodes"><img src="./images/TwitterIcon.png" rel="noreferrer" alt="Twitter Icon"/></a> </li>
+                <li> <a target="_blank" n href="https://twitter.com/CodingKatCodes"><img src="./images/TwitterIcon.png" alt="Twitter Icon"/></a> </li>
                 <li> <a target="_blank" rel="noreferrer" href="https://github.com/KatherineMarcott"><img src="./images/GitHubIcon.png" alt="GitHub Icon"/></a> </li> 
             </ul>
         </footer> 
